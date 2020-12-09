b4e6d706658300d534bac8710633982d39b7d17a
LuckyNew99945 <luckynew99945@gmail.com>
2020-12-09 11:22:40 +0700

    learn basic typescript now learn about assign basic type to function

---
+++ script.js
@@ -0,0 +1,13 @@
+// console.log('hello world from typescript');
+// const inputs = document.querySelectorAll('input');
+// console.log(inputs);
+// inputs.forEach(input => {
+//   console.log(input);
+// })
+var character = 'billy';
+var age = 21;
+var isMale = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7));
---
+++ script.ts
@@ -0,0 +1,19 @@
+// console.log('hello world from typescript');
+
+// const inputs = document.querySelectorAll('input');
+
+// console.log(inputs);
+
+// inputs.forEach(input => {
+//   console.log(input);
+// })
+
+let character = 'billy';
+let age = 21;
+let isMale = true;
+
+const circ = (diameter:number) => {
+  return diameter * Math.PI
+}
+
+console.log(circ(7));
